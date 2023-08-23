3d174305fffe7f1edd18d14f94e7c81e3497d39b
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-22 20:41:23 -0400

    Add files via upload

---
+++ Basic Overview/Giraffe/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName [] = "Jordan";
+    int characterAge = 65;
+
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old\n", characterAge);
+
+    characterAge = 30;
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d\n", characterAge);
+
+    return 0;
+}
