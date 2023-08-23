d78e4b786bbbc3d00b10e3548abaf5da222c3af0
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-22 23:04:32 -0400

    Add files via upload

---
+++ printFFunction/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int favNum = 90;
+    char myChar = 'i';
+    printf("Hello World!!!\n");
+    printf("My favorite %c is %d.", myChar, favNum);
+
+
+    return 0;
+}
