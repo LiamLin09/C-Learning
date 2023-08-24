398212167c254665b39ace317d237a6b306420c7
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-23 22:31:31 -0400

    Add files via upload

---
+++ workingWithNumbers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num = 6;
+    printf("%d\n", num);
+
+    printf("%f\n", pow(4, 3) );
+
+    printf("%f\n", sqrt(36) );
+
+    printf("%f\n", ceil(36.356) );
+
+    printf("%f\n", floor(36.356) );
+
+
+    return 0;
+}
