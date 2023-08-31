b71a1c5d6216ce9676b917535d2df7d46765459f
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-30 20:57:13 -0400

    Add files via upload

---
+++ Structs/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+
+};
+
+int main()
+{
+    struct Student student1;
+    student1.age = 22;
+    student1.gpa = 3.2;
+    strcpy( student1.name, "Jim");
+    strcpy( student1.major, "Business");
+
+    printf("%f", student1.gpa);
+    printf("%s", student1.name);
+
+
+
+    return 0;
+}
